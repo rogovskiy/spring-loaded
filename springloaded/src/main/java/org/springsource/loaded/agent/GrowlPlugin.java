83eb9f6a87a76de213e807069314d5ee813d1d6f
sr <srahouski@idsgrp.com>
2016-08-31 15:09:13 -0500

    Tweaking Growl plugin

--- springloaded/src/main/java/org/springsource/loaded/agent/GrowlPlugin.java
+++ springloaded/src/main/java/org/springsource/loaded/agent/GrowlPlugin.java
@@ -2,6 +2,7 @@ package org.springsource.loaded.agent;
 
 import org.springsource.loaded.ReloadEventProcessorPlugin;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -17,9 +18,9 @@ public class GrowlPlugin implements ReloadEventProcessorPlugin {
   @Override
   public void reloadEvent(String typename, Class<?> clazz, String encodedTimestamp) {
     String growlLocation = System.getProperty("growlnotify.path");
-    if (growlLocation != null) {
+    if (growlLocation != null && new File(growlLocation).canRead()) {
       try {
-        Runtime.getRuntime().exec(new String[] { growlLocation, "/a:SpringLoaded", "/t:Reloaded", "/n:Grails", typename});
+        Runtime.getRuntime().exec(new String[] { growlLocation, "/a:SpringLoaded", "/t:Reloaded", "/n:Reloaded", typename});
       } catch (IOException e) {
         // ignore
       }
